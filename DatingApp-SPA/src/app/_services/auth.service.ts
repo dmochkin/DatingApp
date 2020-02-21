d65c7d05c8e04124dc47d73d9af1daca8d552c07
Dimitry Mochkin <dmochkin@gmail.com>
2020-02-21 16:14:15 -0500

    Added theming and JWT tokenization

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -9,25 +10,33 @@ import { map } from 'rxjs/operators';
 export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-login(model: any) {
-  const requestTarget = this.baseUrl + 'login';
-  const request = this.http.post(requestTarget, model);
-  return request.pipe(map((response: any) => {
-    const user = response;
-    if (user) {
-      localStorage.setItem('token', user.token);
-    }
-  })
-  );
-}
+  login(model: any) {
+    const requestTarget = this.baseUrl + 'login';
+    const request = this.http.post(requestTarget, model);
+    return request.pipe(map((response: any) => {
+      const user = response;
+      if (user) {
+        localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+      }
+    })
+    );
+  }
 
-register(model: any) {
-  const requestTarget = this.baseUrl + 'register';
-  const request = this.http.post(requestTarget, model);
-  return request;
-}
+  register(model: any) {
+    const requestTarget = this.baseUrl + 'register';
+    const request = this.http.post(requestTarget, model);
+    return request;
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 
 }
--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # DatingApp
-Simple Dating App built using:
-- Angular 9
-- .NET Core 3.0
-- MS Entity Framework
 
-Additional plug ins used:
-- Bootstrap
-- Alertifyjs
+Simple Dating App built using:
+- .NET Core 3.0
+- Angular 9.0.1
+
+Additional libraries/plugins used:
+- Bootstrap 4.4.1
+- ngx-bootstrap 5.3.2
+- Bootswatch 4.4.1
+- Alertifyjs 1.13.1
+- Angular JWT 4.0.0
